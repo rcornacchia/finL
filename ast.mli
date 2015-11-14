d8f11b3e36243a52aec2d42ff0c6c13f4b2ddb61
joshfram <jpf2141@columbia.edu>
2015-11-14 18:00:23 -0500

    fixed merge errors

--- ast.mli
+++ ast.mli
@@ -5,4 +5,15 @@ type expression =
   | Var of string
   | Binop of expression * op * expression
   | Assign of string * expression
-  | Call of string * expression list 
+  | Call of string * expression list
+
+type statement =
+  Expr of expression
+
+type func_decl = {
+  fname : string;
+  formals : string list;
+  body : statement list;
+}
+
+type program = string list * func decl_list
