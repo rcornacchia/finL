4b65354b45a585b0180d4b5cf96fabad3aaf55ab
joshfram <jpf2141@columbia.edu>
2015-12-19 00:53:55 -0500

    first java test file

---
+++ src/finL/Test.java
@@ -0,0 +1,30 @@
+package finL;
+
+
+
+public class Test { 
+	
+	public Test() { 
+		
+		
+		
+		
+	}
+	
+	
+	public static boolean num_to_boolean(int x) {
+		if(x != 0) {
+			return true; 
+		}
+		else return false;
+	}
+	
+	public static boolean num_to_boolean(double x) { 
+		if(x != 0) { 
+			return true;
+		}
+		else return false;
+	}
+	
+	
+}
