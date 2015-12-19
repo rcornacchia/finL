244c52fbb88911576c702a1127e008eb1accc10b
Lauren O'Connor <leo2118@columbia.edu>
2015-12-19 00:00:22 -0500

    added bool converter

---
+++ src/java/BooleanConverter.java
@@ -0,0 +1,14 @@
+public class BooleanConverter{
+        public static void main (String args[]){
+
+        }
+
+        static boolean num_to_bool(int x){
+                if(x!=0) return true; else return false;
+        }
+
+        static boolean num_to_bool(double x){
+                if(x!=0) return true; else return false;
+
+        }
+}
