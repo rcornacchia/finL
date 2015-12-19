dfe60774b6f15c9646f0fde8a7d7c71085119c6f
PaddyQuinn <leo2118@columbia.edu>
2015-12-19 16:05:20 -0500

    test.java

--- src/java/test/Test.java
+++ src/java/test/Test.java
@@ -1,9 +1,18 @@
+<<<<<<< Updated upstream
 package test;
+=======
+<<<<<<< Updated upstream
+package java.test;
+=======
+package bin.test;
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 
 
 public class Test { 
 	
+<<<<<<< Updated upstream
 	public Test() { 
 		
 		
@@ -26,5 +35,42 @@ public class Test {
 		else return false;
 	}
 	
+=======
+	public static boolean num_to_boolean(int x) {
+		if(x != 0) { return true; }
+		else { return false; }
+	}
+	
+	public static boolean num_to_boolean(double x) { 
+		if (x != 0) { return true; }
+		else { return false; }
+	}
+
+	public static int boolean_to_int(boolean b) {
+		if (b) { return 1; }
+		else { return 0; }
+	}
+	
+	public static boolean compare_strings(String s1, String op, String s2) {
+		if (s1.equals(s2)) {
+			switch (op) {
+				case "==":	return true;
+				case "<":	return false; 	
+				case "<=":	return true;
+				case ">":	return false;
+				case ">=":	return true;
+			}
+		} else {
+			switch (op) {
+				case "==":	return false;
+				case "<": 	if (s1.compareTo(s2) < 0) { return false; } else { return true; }
+				case "<=": 	if (s1.compareTo(s2) < 0) { return false; } else { return true; }
+				case ">":	if (s1.compareTo(s2) > 0) { return false; } else { return true; }
+				case ">=":	if (s1.compareTo(s2) > 0) { return false; } else { return true; }
+			}
+		}
+		return false;
+	}
+>>>>>>> Stashed changes
 	
 }
